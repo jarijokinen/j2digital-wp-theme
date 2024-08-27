a9b0b2050ed1566df3bfc06502f6e10e2af6a8ab
Jari Jokinen <jari@j2digital.io>
2024-08-28 01:00:52 +0300

    Add support for asset enqueuing

--- dist/functions.php
+++ dist/functions.php
@@ -2,6 +2,7 @@
 
 namespace j2digital;
 
+require_once get_theme_file_path('functions/enqueue-assets.php');
 require_once get_theme_file_path('functions/site-title.php');
 
 ?>
--- src/php/functions.php
+++ src/php/functions.php
@@ -2,6 +2,7 @@
 
 namespace <%= slug %>;
 
+require_once get_theme_file_path('functions/enqueue-assets.php');
 require_once get_theme_file_path('functions/site-title.php');
 
 ?>
